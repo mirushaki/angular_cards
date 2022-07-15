d0b19133952c8d569de3a543ce44da919d227516
David_Qobuladze <davidkobuladzemail@gmail.com>
2022-07-15 21:56:04 +0400

    Makes comparision between letters and create special classes for spans

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,5 +14,12 @@ export class AppComponent {
     this.enteredText = value;
   }
   
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return "pending";
+    }
+
+    return (randomLetter === enteredLetter) ? "correct" : "incorrect";
+  }
 
 }
