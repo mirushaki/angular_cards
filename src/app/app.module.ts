cc23b7617c6f7f6d9d062ab681ecfe518e83d29b
mirushaki <mirianjinchvelashvili@gmail.com>
2022-08-05 00:00:52 +0400

    Create modules and corresponding home components for each module, import created modules in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CollectionsModule } from './collections/collections.module';
+import { ElementsModule } from './elements/elements.module';
+import { ModsModule } from './mods/mods.module';
+import { ViewsModule } from './views/views.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ElementsModule,
+    CollectionsModule,
+    ViewsModule,
+    ModsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
