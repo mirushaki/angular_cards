b19b344c6762eef171aa6737e757e28bad5cbb43
mirushaki <mirianjinchvelashvili@gmail.com>
2022-08-04 23:25:24 +0400

    Initial empty project with angular routing

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# AngularCards
+# Comps
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.5.
 
 ## Development server
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular_cards'),
+      dir: require('path').join(__dirname, './coverage/comps'),
       subdir: '.',
       reporters: [
         { type: 'html' },
