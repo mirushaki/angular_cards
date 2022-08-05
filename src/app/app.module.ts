ee7030dc7ef4acb0802535be5c82dd83cb0b37b5
David_Qobuladze <davidkobuladzemail@gmail.com>
2022-08-05 22:09:15 +0400

    Modules examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CtFPipe } from './ct-f.pipe';
+import { CollectionsModule } from './collections/collections.module';
+import { ElementsModule } from './elements/elements.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { CtFPipe } from './ct-f.pipe';
     CtFPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CollectionsModule,
+    ElementsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
