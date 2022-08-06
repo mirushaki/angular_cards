01c9f4f5819e0664a87ed8e7ebc364292d51c99b
mirushaki <mirianjinchvelashvili@gmail.com>
2022-08-06 20:59:17 +0400

    Refactor home page and not-found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { CollectionsModule } from './collections/collections.module';
 import { ElementsModule } from './elements/elements.module';
 import { ModsModule } from './mods/mods.module';
 import { ViewsModule } from './views/views.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    ModsModule,
     ElementsModule,
     CollectionsModule,    
     ViewsModule,
-    ModsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
