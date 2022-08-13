4da834fbdff553fd7e9add12bdf33f2e8cc2de66
David_Qobuladze <davidkobuladzemail@gmail.com>
2022-08-13 14:22:55 +0400

    changs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CollectionsModule } from './collections/collections.module';
-import { ElementsModule } from './elements/elements.module';
-import { ModsModule } from './mods/mods.module';
-import { ViewsModule } from './views/views.module';
-import { WeatherBulmaModule } from './weather-bulma/weather-bulma.module';
-import { WeatherModule } from './weather/weather.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+// import { CollectionsModule } from './collections/collections.module';
+// import { ElementsModule } from './elements/elements.module';
+// import { ModsModule } from './mods/mods.module';
+// import { ViewsModule } from './views/views.module';
+// import { WeatherBulmaModule } from './weather-bulma/weather-bulma.module';
+// import { WeatherModule } from './weather/weather.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ElementsModule,
-    CollectionsModule,    
-    ViewsModule,
-    ModsModule,
+    // ElementsModule,
+    // CollectionsModule,    
+    // ViewsModule,
+    // ModsModule,
     // WeatherBulmaModule,
-    WeatherModule
+    // WeatherModule
   ],
   providers: [],
   bootstrap: [AppComponent]
